97399c77dcd10c42ea834bafd231ba1efc21e118
afifarn <afifr0837@gmail.com>
2019-09-25 15:51:26 +0700

    first commit

---
+++ src/main/java/Latihan13_Relational/Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan13_Relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk Penggunaan Relasi
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b =" + (bil1 > bil2));
+        System.out.println("a < b =" + (bil1 < bil2));
+        System.out.println("b >= a =" + (bil1 >= bil2));
+        System.out.println("b <= a =" + (bil1 <= bil2));
+    }
+    
+}
